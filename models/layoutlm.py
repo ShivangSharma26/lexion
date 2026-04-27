19be893e37ed09015a446ba79106a13185222f6b
ShivangSharma26 <iamluckysharma99@gmail.com>
2026-04-27 15:48:00 +0530

    refactor: restructure repository to separate backend and frontend

--- models/layoutlm.py
+++
@@ -1,9 +0,0 @@
-from transformers import LayoutLMForTokenClassification
-
-def get_layoutlm_model(model_name, num_labels):
-    # LayoutLM understands both text and layout (bounding boxes).
-    model = LayoutLMForTokenClassification.from_pretrained(
-        model_name,
-        num_labels=num_labels
-    )
-    return model
